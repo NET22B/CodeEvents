0fa9e9166fa21b1a29034b94c2b3d5e6b5a85607
DZM77 <dimitris.bjorlingh@outlook.com>
2022-09-02 12:50:52 +0200

    Get and create with httprequest message

--- CodeEvents.Client/Controllers/HomeController.cs
+++ CodeEvents.Client/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using CodeEvents.Common.Dto;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace CodeEvents.Client.Controllers
@@ -9,18 +10,61 @@ namespace CodeEvents.Client.Controllers
     public class HomeController : Controller
     {
         private HttpClient httpClient;
+        private const string json = "application/json";
 
         public HomeController()
         {
             
            httpClient = new HttpClient();
            httpClient.BaseAddress = new Uri("https://localhost:7150");
+          // httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         public async Task<IActionResult> Index()
         {
-            var res = await SimpleGet();
+            //var res = await SimpleGet();
+            //var res = await GetWithRequestMessage();
+            var res = await CreateLecture();
+
             return View();
+        }
+
+        private async Task<LectureDto> CreateLecture()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "api/events/Gruppen1/lectures");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
+
+            var lecture = new LectureCreateDto
+            {
+                Level = 500,
+                Title = "From Client"
+            };
+
+            var serializedLecture = JsonSerializer.Serialize(lecture);
+
+            request.Content = new StringContent(serializedLecture);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue(json);
+
+            var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsStringAsync();
+            var codeEvents = JsonSerializer.Deserialize<LectureDto>(result, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            return codeEvents!;
+
+        }
+
+        private async Task<IEnumerable<CodeEventDto?>> GetWithRequestMessage()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/events");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
+
+            var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsStringAsync();
+            var codeEvents = JsonSerializer.Deserialize<IEnumerable<CodeEventDto>>(result, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            return codeEvents!;
         }
 
         private async Task<IEnumerable<CodeEventDto?>> SimpleGet()
@@ -30,7 +74,7 @@ namespace CodeEvents.Client.Controllers
 
             var result = await response.Content.ReadAsStringAsync();
             var codeEvents = JsonSerializer.Deserialize<IEnumerable<CodeEventDto>>(result, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-
+            
             return codeEvents!;
         }
 
